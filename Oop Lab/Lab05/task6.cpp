91b480654bb35b7c202bb926328aee96ce1ce7ba
Sahil Latif <144492231+Sahillatif0@users.noreply.github.com>
2024-02-27 05:48:55 +0000

    Lab06 task6

---
+++ Oop Lab/Lab05/task6.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class GraphicsEngine{
+
+};
+class InputHandler{
+
+};
+class PhysicsEngine{
+
+};
+class GameEngine{
+    GraphicsEngine ge;
+    PhysicsEngine pe;
+    InputHandler ih;
+
+};
+
+int main(){
+    
+}
